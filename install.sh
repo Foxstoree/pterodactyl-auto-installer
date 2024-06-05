bc1dd24cc00ce656687f4e8514b99fb3debcc95e
Foxstoree <foxxstoredigital@gmail.com>
2024-06-05 23:53:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Warna
-BLUE='\033[0;34m'         # Blue
+BLUE='\033[0;34m'       
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 echo -e "                                                       "
 echo -e "                                                       "
