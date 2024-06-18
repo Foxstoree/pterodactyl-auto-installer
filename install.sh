a0240807cbb4fcdc9141a31a55fd49ac7b4e07f3
Foxstoree <foxxstoredigital@gmail.com>
2024-06-18 18:40:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,6 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
-# Cek Ram
-available_ram=$(grep MemAvailable /proc/meminfo | awk '{print $2}')
-available_ram_mb=$((available_ram / 1024))
-
 # Display welcome message
 display_welcome() {
   echo -e ""
@@ -34,11 +30,6 @@ display_welcome() {
   clear
 }
 
-#Melakukan Pengecekan
-if (( available_ram_mb < 2048 )); then
-  echo -e "${RED}RAM yang tersedia kurang dari 4GB. Script tidak akan dijalankan.${NC}"
-  exit 1
-fi
 #Update and install jq
 install_jq() {
   echo -e "                                                       "
